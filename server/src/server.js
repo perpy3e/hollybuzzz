ced762d4cef3461df97cfc459ed3ac9f638b9177
lbib <rhitakane@gmail.com>
2024-12-13 06:44:40 +0700

    server.js to src

--- server/src/server.js
+++ server/src/server.js
@@ -2,9 +2,9 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
-import connectDB from "./config/mongodb.js"; // connect db
-import authRouter from "./routes/authRoutes.js"; // router user
-import userRouter from "./routes/userRoutes.js"; //user name/verify
+import connectDB from "../config/mongodb.js"; // connect db
+import authRouter from "../routes/authRoutes.js"; // router user
+import userRouter from "../routes/userRoutes.js"; //user name/verify
 
 
 const app = express();
