890c65d0b51e1cd41e1d36e2d9b4a2211b2281aa
lbib <rhitakane@gmail.com>
2024-12-13 10:26:04 +0700

    Update userAuth.js

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -4,9 +4,7 @@ import jwt from "jsonwebtoken";
 const userAuth = async (req, res, next) => {
    const { token } = req.cookies;
 
-   if (token) {
-       return res.json({ success: false, message: 'Not Authorized. Login Again' });
-   }
+  console.log(token)
 
    try {
        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
