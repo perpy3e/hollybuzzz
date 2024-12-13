6e3efad3da999a3529214afaf87b5242d81abfa1
lbib <rhitakane@gmail.com>
2024-12-13 08:51:38 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // This will create a 'build' directory instead of 'dist'
+    chunkSizeWarningLimit: 1000 // Addresses the chunk size warning
+  }
 })
