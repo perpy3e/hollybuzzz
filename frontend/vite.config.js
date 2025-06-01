be8be0be2024d62767229b91c0f9637246228942
lbib <rhitakane@gmail.com>
2025-06-02 01:56:12 +0700

    Revert "-"
    This reverts commit 657193bcc61ad7851efbc56c8c58e26ae055b69b.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../server/public',  // output build here relative to frontend folder
-    emptyOutDir: true,           // clean old builds
-    chunkSizeWarningLimit: 1000,
+    outDir: 'build', // This will create a 'build' directory instead of 'dist'
+    chunkSizeWarningLimit: 1000 // Addresses the chunk size warning
   }
-});
+})
