e916edad247245debeb7590eaea26629fd2a8387
lbib <rhitakane@gmail.com>
2025-06-17 22:43:31 +0700

    Update userAuth.js

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -2,26 +2,26 @@ import jwt from "jsonwebtoken";
 
 
 const userAuth = async (req, res, next) => {
-   const { token } = req.cookies;
+    const { token } = req.cookies;
 
-  console.log(token)
+    console.log(token)
 
-  if (!token) {
-    return res.json({ success: false, message: 'Not Authorized. Non-Token' });
-  }
-   try {
-       const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
-       if (tokenDecode.id) {
-           req.body.userId = tokenDecode.id;
-       } else {
-           return res.json({ success: false, message: 'Not Authorized. Login Again' });
-       }
+    if (!token) {
+        return res.json({ success: false, message: 'Not Authorized. Non-Token' });
+    }
+    try {
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+        if (tokenDecode.id) {
+            req.body.userId = tokenDecode.id;
+        } else {
+            return res.json({ success: false, message: 'Not Authorized. Login Again' });
+        }
 
-      next();
+        next();
 
-   } catch (error) {
-       res.json({ success: false, message: error.message }); 
-   }
+    } catch (error) {
+        res.json({ success: false, message: error.message });
+    }
 }
 
 
